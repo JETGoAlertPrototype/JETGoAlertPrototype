d3395d7c3309eabf7e1a6973570a912fa3b2abf9
JETGoAlertPrototype <digiartsacct@gmail.com>
2025-03-11 07:05:55 +0800

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,18 @@
+import { checkLogin, logoutUser } from "./auth.js";
+import { fetchEarthquakeData } from "./earthquake.js";
+import { initMap } from "./maps.js";
+import { analyzeLatestEarthquake } from "./ai_risk.js";
+import { toggleDarkMode } from "./darkmode.js";
+import { sendEmergencySMS } from "./sms.js";
+import { startQuiz } from "./quiz.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+    checkLogin();
+    fetchEarthquakeData();
+    setInterval(fetchEarthquakeData, 30000);
+});
+
+document.getElementById("logoutButton")?.addEventListener("click", logoutUser);
+document.getElementById("darkModeToggle")?.addEventListener("click", toggleDarkMode);
+document.getElementById("sendSMSButton")?.addEventListener("click", sendEmergencySMS);
+document.getElementById("startQuizButton")?.addEventListener("click", startQuiz);
