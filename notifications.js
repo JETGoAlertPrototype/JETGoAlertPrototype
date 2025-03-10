d59edeb8efa7d576006eb6d27d091e5738d69154
JETGoAlertPrototype <digiartsacct@gmail.com>
2025-03-11 07:37:22 +0800

    Create notifications.js

---
+++ notifications.js
@@ -0,0 +1,6 @@
+// notifications.js - Send Emergency SMS & Email Alerts
+function sendEmergencySMS() {
+    alert("Emergency SMS sent to all registered contacts!");
+}
+
+document.getElementById("sendSMSButton")?.addEventListener("click", sendEmergencySMS);
