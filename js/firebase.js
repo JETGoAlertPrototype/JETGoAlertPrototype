e1b4345093ae5b022f829f7a68c8e77b0e2d8542
JETGoAlertPrototype <digiartsacct@gmail.com>
2025-03-11 09:17:07 +0800

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,13 +1,12 @@
-// Import Firebase modules
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-firestore.js";
+import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-firestore.js";
 
 // Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyB_0CodzHFyERYxy2Apy8Zp4QbHsbr4rfk",
     authDomain: "jet-go-alert.firebaseapp.com",
     projectId: "jet-go-alert",
-    storageBucket: "jet-go-alert.appspot.com",  // Fix storage bucket URL
+    storageBucket: "jet-go-alert.appspot.com",
     messagingSenderId: "605131486515",
     appId: "1:605131486515:web:ae9661d467e1f63725ba46",
     measurementId: "G-LF30TWJ3YS"
@@ -18,4 +17,4 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 // Export Firestore database
-export { db };
+export { db, collection, addDoc };
