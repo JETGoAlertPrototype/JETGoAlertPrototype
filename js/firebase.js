c2f524ed2fb2450f20cb208cb7b18ab87846c1dd
JETGoAlertPrototype <digiartsacct@gmail.com>
2025-03-12 05:58:16 +0800

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
-import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-firestore.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-firestore.js";
 
 // Firebase configuration
 const firebaseConfig = {
